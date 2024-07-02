b342ec5d6b98ccdbc2cc397dc71ddec63903a20f
4FlamePrincess4 <laurelsindewald@gmail.com>
2024-07-02 15:59:50 -0600

    Update run_r_script.sh

--- run_r_script.sh
+++ run_r_script.sh
@@ -7,26 +7,21 @@
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --ntasks=64
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=1
 #SBATCH --output=FOA2c_r10_tofu_comb_AD_tifs_slurmlog_%A.out
 #SBATCH --error=FOA2c_r10_tofu_comb_AD_tifs_error_%A.log
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=laurel.sindewald@ucdenver.edu
 
+# Change to the directory containing your R script
+cd /data001/projects/sindewal/FSim_post_processing/
+
 # Load necessary modules 
 R
 
-# Define the list of R packages needed
-R_PACKAGES=("tidyverse" "sf" "terra" "tidyterra" "RSQLite" "future" "furrr" "log4r")
-
-# Loop through each package and install it if not already installed
-for pkg in "${R_PACKAGES[@]}"; do
-    library($pkg)
-done
-echo "Done loading packages."
-
-# Change to the directory containing your R script
-cd /data001/projects/sindewal/FSim_post_processing/
+# Specify the first and last seasons for this job.
+first_season <- 1
+last_season <- 2500
 
 # Run the R script
 echo "Running R script..."
