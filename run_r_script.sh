662c98fb197976b3b883fb0e6075e9abaa53029b
4FlamePrincess4 <laurelsindewald@gmail.com>
2024-07-02 15:36:15 -0600

    Update run_r_script.sh

--- run_r_script.sh
+++ run_r_script.sh
@@ -6,33 +6,28 @@
 #SBATCH --partition=math-alderaan
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --ntasks=1
+#SBATCH --ntasks=64
 #SBATCH --cpus-per-task=16
 #SBATCH --output=FOA2c_r10_tofu_comb_AD_tifs_slurmlog_%A.out
 #SBATCH --error=FOA2c_r10_tofu_comb_AD_tifs_error_%A.log
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=laurel.sindewald@ucdenver.edu
 
-# Load necessary modules (if needed)
-module load R
+# Load necessary modules 
+R
 
 # Define the list of R packages needed
-R_PACKAGES=("tidyverse" "sf" "sp" "raster" "terra" "tidyterra" "RSQLite")
+R_PACKAGES=("tidyverse" "sf" "terra" "tidyterra" "RSQLite" "future" "furrr" "log4r")
 
 # Loop through each package and install it if not already installed
 for pkg in "${R_PACKAGES[@]}"; do
-    if ! R -e "library($pkg)" &>/dev/null; then
-        echo "Installing $pkg..."
-        R -e "install.packages('$pkg', repos='https://cran.rstudio.com/')"
-    else
-        echo "$pkg is already installed."
-    fi
+    library($pkg)
 done
-echo "Done loading and installing packages."
+echo "Done loading packages."
 
 # Change to the directory containing your R script
 cd /data001/projects/sindewal/FSim_post_processing/
 
 # Run the R script
 echo "Running R script..."
-Rscript FSim_post-processing1_merge_fire_tifs.R
+Rscript FSim_post_processing1_merge_fire_tifs_v10_refactored.R
