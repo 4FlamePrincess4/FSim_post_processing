9ae4c6e0e9688c27e51eb0bd3072fdf74581b69f
4FlamePrincess4 <laurelsindewald@gmail.com>
2024-07-05 12:19:39 -0600

    Update run_r_script.sh

--- run_r_script.sh
+++ run_r_script.sh
@@ -5,8 +5,8 @@
 #SBATCH --job-name=contifs
 #SBATCH --partition=math-alderaan
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
 #SBATCH --output=FOA2c_r10_comb_AD_tifs_slurmlog_%A.out
 #SBATCH --error=FOA2c_r10_comb_AD_tifs_error_%A.log
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -21,4 +21,13 @@ mkdir ../okwen_foa2c_r10/SeasonFires_merged_tifs/
 source /data001/projects/sindewal/anaconda3/bin/activate r_env
 
 # Run the R script
-/data001/projects/sindewal/anaconda3/envs/r_env/bin/Rscript FSim_post_processing1_merge_fire_tifs_v10_refact_slurm.R --foa_lcp_path ./_inputs/lcp/FOA2c_LCG_LF2022_FBFM40_230_120m.tif --working_directory /data001/projects/sindewal/okwen_foa2c_r10/ --foa_run FOA2c_r10 --scenario "" --run_timepoint baseline_time0 --number_of_seasons 20000 --seasons_in_part 2500 --number_of_parts 8 --first_season 1 --last_season 2500 
+/data001/projects/sindewal/anaconda3/envs/r_env/bin/Rscript FSim_post_processing1_merge_fire_tifs_v10_refact_slurm.R \
+--foa_lcp_path ./_inputs/lcp/FOA2c_LCG_LF2022_FBFM40_230_120m.tif \
+--working_directory /data001/projects/sindewal/okwen_foa2c_r10/ \
+--foa_run FOA2c_r10 --scenario "" \
+--run_timepoint baseline_time0 \
+--number_of_seasons 20000 \
+--seasons_in_part 2500 \
+--number_of_parts 8 \
+--first_season 1 \
+--last_season 2500 
