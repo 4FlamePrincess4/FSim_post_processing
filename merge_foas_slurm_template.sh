84dd3412b81dcde19ac52b6a519b85ad5fa700fe
4FlamePrincess4 <laurelsindewald@gmail.com>
2024-11-27 11:56:21 -0700

    Create merge_foas_slurm_template.sh
    This slurm script serves as a template for combining the burn probability rasters of a run.

---
+++ merge_foas_slurm_template.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Note: before running on Linux, you may need to run this line to remove stupid Windows characters:
+# sed -i -e 's/\r$//' merge_run_parts_slurm_template.sh
+
+#SBATCH --job-name=122RO3_merge_foas
+#SBATCH --partition=short
+#SBATCH --time=02-00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=96
+#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3/foa1c_r16_LF2022_RecOff3_merge_foas_slurmlog_%A.out
+#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3/foa1c_r16_LF2022_RecOff3_merge_foas_error_%A.log
+#SBATCH --mail-user=laurel.sindewald@usda.gov
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#Change to the directory containing your R script
+cd /project/wildland_fire_smoke_tradeoff/FSim_post_processing/
+
+#Activate conda environment
+module load miniconda
+source activate r_env2
+
+#Run the R script
+/home/laurel.sindewald/.conda/envs/r_env2/bin/Rscript FSim_post_processing4_merge_foas.R \
+--working_directory /project/wildland_fire_smoke_tradeoff/ \
+--scenario LF2022_RecOff3 \
+--run_timepoint time2 \
+--study_area_lcp ./study_area_lcps/LF2020_220_OKAWEN_Colville_LCP_120m.tif
