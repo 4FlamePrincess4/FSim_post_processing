0da7a96eebc39a93d60b5cd94286a5a68d3b8f41
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-02-14 13:17:37 -0700

    Create contifs_example_SCINet.sh

---
+++ contifs_example_SCINet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Note: Before running this on Linux, you'll need to run this line to remove stupid Windows characters:
+# sed -i -e 's/\r$//' rename_tifs.sh
+
+#SBATCH --job-name=122RO3con
+#SBATCH --partition=short
+#SBATCH --time=02-00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=96
+#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3_2/FOA1c_r16_2022_RecOff3_2_comb_tifs_slurmlog_%A.out
+#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3_2/FOA1c_r16_2022_RecOff3_2_comb_tifs_error_%A.log
+#SBATCH --mail-user=laurel.sindewald@usda.gov
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# Change to the directory containing your R script
+cd /project/wildland_fire_smoke_tradeoff/FSim_post_processing/
+# Make a directory to hold the merged tifs
+mkdir ../okwen_foa1c_r16_LF2022_RecOff3_2/SeasonFires_merged_tifs_LF2022_RecOff3_2/
+
+# Activate conda environment
+module load miniconda
+source activate r_env2
