247e7dab30ae12537aca6d6853f13ec9ed4db883
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-03-25 10:16:25 -0600

    Update recalc_prob_rasters_cluster.R
    This fix directs the temporary tif files to their own directory to keep the folder tidy until the script finishes, then deletes the directory.

--- recalc_prob_rasters_cluster.R
+++ recalc_prob_rasters_cluster.R
@@ -34,6 +34,10 @@ season_fire_rasters <- vrt(season_fire_files)
 prop_non_na <- app(season_fire_rasters, function(x) mean(!is.na(x)), filename=paste0("./recalc_", opt$foa_run, "_", opt$scenario, "_", opt$run_timepoint, "_burn_probability.tif"), overwrite=TRUE)
 names(prop_non_na) <- "burn_probability"
 
+# Create a temporary directory for storing intermediate tifs
+temp_dir <- file.path(wd, "temp_tifs")
+dir.create(temp_dir, showWarnings = FALSE)
+
 # Process flame length probabilities efficiently
 categories <- list(
   "0_2"   = c(0, 2),
@@ -60,7 +64,7 @@ for (cat in names(categories)) {
     })
     
     # Save the result and add to the list
-    filename_temp <- paste0("temp_", i, ".tif")
+    filename_temp <- file.path(temp_dir, paste0("temp_", i, ".tif"))
     writeRaster(category_count_raster, filename_temp, overwrite=TRUE)
     category_count_list[[i]] <- rast(filename_temp)
   }
@@ -74,7 +78,7 @@ for (cat in names(categories)) {
   probability_raster <- category_count_raster / prop_non_na
   writeRaster(probability_raster, paste0("recalc_", opt$foa_run, "_", opt$scenario, "_", opt$run_timepoint, "_flp_flame_length_", cat, ".tif"), overwrite=TRUE)
   
-  # Cleanup temporary files
-  file.remove(list.files(pattern="temp_.*tif$"))
+  # Cleanup: Delete all temporary files and remove the directory
+  unlink(temp_dir, recursive=TRUE)
 }
 
