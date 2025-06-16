4c7ca865a16ca93c346163d36af594099d929b67
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-06-16 17:15:03 -0600

    Update recalc_prob_rasters_SCINet.sh

--- recalc_prob_rasters_SCINet.sh
+++ recalc_prob_rasters_SCINet.sh
@@ -2,13 +2,13 @@
 # Note: Before running this on Linux, you'll need to run this line to remove stupid Windows characters:
 # sed -i -e 's/\r$//' rename_tifs.sh
 
-#SBATCH --job-name=122RO3recalc
+#SBATCH --job-name=F1RO3rec
 #SBATCH --partition=ceres
-#SBATCH --time=02-00:00
+#SBATCH --time=04-00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=96
-#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3_2/FOA1c_r16_2022_RecOff3_2_recalc_prob_rasters_slurmlog_%A.out
-#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okwen_foa1c_r16_LF2022_RecOff3_2/FOA1c_r16_2022_RecOff3_2_recalc_prob_rasters_error_%A.log
+#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r16_LF2020_RO2020_3/_error/foa1c_r16_LF2020_RO2020_3_recalc_prob_rasters_slurmlog_%A.out
+#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r16_LF2020_RO2020_3/_error/foa1c_r16_LF2020_RO2020_3_recalc_prob_rasters_error_%A.log
 #SBATCH --mail-user=laurel.sindewald@usda.gov
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -23,8 +23,9 @@ module load miniconda
 source activate r_env2
 
 # Run the R script
-/home/laurel.sindewald/.conda/envs/r_env2/bin/Rscript recalc_prob_rasters_cluster.R \
---working_directory /data001/projects/sindewal/okwen_foa1c_r16_LF2020/ \
+/home/laurel.sindewald/.conda/envs/r_env2/bin/Rscript FSim_post_processing6_recalc_prob_rasters_accumulator_cluster_parallel_5.R \
+--working_directory /project/wildland_fire_smoke_tradeoff/okawen_foa1c_r16_LF2020_RO2020_3/ \
+--foa_lcp_path ./_inputs/lcp/lf2020_220_foa1c_120m.tif \
 --foa_run foa1c_r16 \
---scenario LF2020 \
---run_timepoint baseline_time0
+--scenario LF2020_RO2020_3 \
+--run_timepoint time0
