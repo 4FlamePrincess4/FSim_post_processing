b97f12682058131305c3cfc5c469ea4f8e8150a2
4FlamePrincess4 <laurelsindewald@gmail.com>
2025-06-23 17:10:24 -0600

    Update merge_foas_slurm_example_SCINet.sh

--- merge_foas_slurm_example_SCINet.sh
+++ merge_foas_slurm_example_SCINet.sh
@@ -3,6 +3,7 @@
 # sed -i -e 's/\r$//' merge_run_parts_slurm_template.sh
 
 #SBATCH --job-name=122RO3_merge_foas
+#SBATCH --account=wildland_fire_smoke_tradeoff
 #SBATCH --partition=ceres
 #SBATCH --time=02-00:00
 #SBATCH --nodes=1
