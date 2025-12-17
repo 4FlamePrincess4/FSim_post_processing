702a938d7f5f1e8b330e9604ee9f21d7046f93a9
Laurel Sindewald <laurelsindewald@gmail.com>
2025-12-17 11:17:54 -0700

    Update delete_unzipped_firetif_subdirectories.sh

--- delete_unzipped_firetif_subdirectories.sh
+++ delete_unzipped_firetif_subdirectories.sh
@@ -2,18 +2,20 @@
 # Note: Before running this on Linux, you'll need to run this line to remove stupid Windows characters:
 # sed -i -e 's/\r$//' rename_tifs.sh
 
-#SBATCH --job-name=120BOzip
+#SBATCH --job-name=120BOcln
 #SBATCH --account=wildland_fire_smoke_tradeoff
 #SBATCH --partition=ceres
 #SBATCH --time=02-00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=96
-#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r17_LF2020_TM_baseline_time0/_error/foa1c_r17_LF2020_TM_baseline_time0_zip_tifs_slurmlog_%A.out
-#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r17_LF2020_TM_baseline_time0/_error/foa1c_r17_LF2020_TM_baseline_time0_zip_tifs_error_%A.log
+#SBATCH --output=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r17_LF2020_TM_baseline_time0/_error/foa1c_r17_LF2020_TM_baseline_time0_cleanup_slurmlog_%A.out
+#SBATCH --error=/project/wildland_fire_smoke_tradeoff/okawen_foa1c_r17_LF2020_TM_baseline_time0/_error/foa1c_r17_LF2020_TM_baseline_time0_cleanup_error_%A.log
 #SBATCH --mail-user=laurel.sindewald@usda.gov
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+
+cd ..
 
 set -euo pipefail
 
@@ -28,4 +30,5 @@ for dir in *_FlameLengths *_ArrivalDays *_ArrivalTimes; do
   else
     echo "[SKIP] No zip found for: $dir"
   fi
+
 done
